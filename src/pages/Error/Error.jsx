5864a9002336fd5421651338b02d16a2c539a9ab
AhmedAshour88 <ahmed.elsayed.ashour@gmail.com>
2025-10-08 01:35:54 +0300

    Update Error Page.jsx

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -4,6 +4,10 @@ import styles from './Error.module.css';
 const Error = () => {
     return (
         <>
+            <main>
+                <h1>An error occured!</h1>
+                <p>Couldn't find this page!</p>
+            </main>
         </>
     )
 }
