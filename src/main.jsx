75699f6d8a09597ade3a5ba636238e70e8a11edd
Abdo0oAdel <3bdo0o3adel@gmail.com>
2025-10-11 18:36:07 +0300

    Restructure App to include home as main app component, set implement for dark mode in variables.css, upload .svg logo, depend on vite tailwind instead of postcss.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import "./assets/styles/styles.css";
 import App from "./App.jsx";
-import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
       <App />
